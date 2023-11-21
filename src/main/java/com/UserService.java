57ef211bb4a447b5f88953336716b402c83fbded
WPTENG396\José Neto <jose.neto@alticeusa.com>
2023-11-20 23:31:04 -0300

    Solução Consultas Parametrizadas (Prepared Statements)

--- src/main/java/com/UserService.java
+++ src/main/java/com/UserService.java
@@ -1,44 +1,25 @@
 package com;
 
-import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 
 @Service
 public class UserService {
-    @Autowired
-    private JdbcTemplate jdbcTemplate;
-    
-    private static final Logger logger = LoggerFactory.getLogger(UserService.class);
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
 
-    public String authenticate(String username, String password) {
-    	logger.info("****************************************************");
-    	logger.info("Data: "+ LocalDate.now()+"; username = " + username);
-    	logger.info("Data: "+ LocalDate.now()+"; password = " + password);
-    	logger.info("****************************************************");
-    	
-        String userSql = "SELECT * FROM user WHERE username = '" + username + "'";
-        List<User> users = jdbcTemplate.query(userSql, 
-            (rs, rowNum) -> new User(rs.getLong("id"), rs.getString("username"), rs.getString("password")));
+	public String authenticate(String username, String password) {
+		String sql = "SELECT * FROM user WHERE username = ? AND password = ?";
+		@SuppressWarnings("deprecation")
+		List<User> users = jdbcTemplate.query(sql, new Object[] { username, password },
+				(rs, rowNum) -> new User(rs.getLong("id"), rs.getString("username"), rs.getString("password")));
 
-        if (users.isEmpty()) {
-            return "Usuário não encontrado";
-        }
-
-        String passwordSql = "SELECT * FROM user WHERE username = '" + username + "' AND password = '" + password + "'";
-        List<User> validUsers = jdbcTemplate.query(passwordSql, 
-            (rs, rowNum) -> new User(rs.getLong("id"), rs.getString("username"), rs.getString("password")));
-
-        if (validUsers.isEmpty()) {
-            return "Senha incorreta";
-        }
-
-        return "Usuário autenticado com sucesso!";
-    }
+		if (users.isEmpty()) {
+			return "Usuário ou senha incorretos";
+		}
+		return "Usuário autenticado com sucesso";
+	}
 }
